8cc66c61b4db1a0843e72d8853a5730f6c8d1495
Mayank Dwivedi <mayankdwivedi@Mayanks-MacBook-Pro.local>
2021-07-24 00:15:27 +0530

    explained about var and int

---
+++ chapter 1 - Variables and values/index.js
@@ -0,0 +1,20 @@
+/**
+ * in this we will be having understanding of how variables and values work in Java Script
+ * olive #bada55
+ * green - 17df53
+ * red - ff0000
+ * white - #ffffff
+ */
+
+// Variable Init
+
+var name = "Javascript new" ;
+let imporvedName = "I am doing better";
+
+var name = "My name will be here";
+imporvedName = "My name is Mayank/shashank"
+
+const staticValue = "ALPHA";
+
+
+ console.log('%c hdd', 'background: #222; color: #bada55');
